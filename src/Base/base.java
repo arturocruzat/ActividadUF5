7e0450e6a3d8e2230157e6f54595069365fe5f6c
arturo cruzat <arturo.cruzat@gmail.com>
2020-05-30 18:17:09 +0200

    Definimos las listas y el scanner

--- src/Base/base.java
+++ src/Base/base.java
@@ -1,9 +1,17 @@
 package Base;
 
+import java.util.ArrayList;
+import java.util.Scanner;
+
 public class base {
+    
+    private static ArrayList<Double> Radios = new ArrayList<Double>();
+    private static ArrayList<Circulo> Circulos = new ArrayList<Circulo>();
+    private static Scanner input = new Scanner(System.in);
     
     public static void main(String[] args) {
         
+
     }
     
     public double getRadio() {
