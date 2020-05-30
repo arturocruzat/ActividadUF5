b4753b8ec60d5527cd06703342e7ccc481e1fe8a
arturo cruzat <arturo.cruzat@gmail.com>
2020-05-30 18:10:08 +0200

    Creamos la funcion getRandom

--- src/Base/base.java
+++ src/Base/base.java
@@ -1,6 +1,12 @@
-
 package Base;
 
 public class base {
     
+    public static void main(String[] args) {
+        
+    }
+    
+    public double getRadio() {
+        return Math.random();
+    }
 }
