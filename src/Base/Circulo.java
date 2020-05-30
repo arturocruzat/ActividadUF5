f469ef8e1558e94b6b8fbdd242685c982c9faa24
arturo cruzat <arturo.cruzat@gmail.com>
2020-05-30 17:51:08 +0200

    Clase circulo creada

---
+++ src/Base/Circulo.java
@@ -0,0 +1,36 @@
+package Base;
+
+public class Circulo {
+    
+    private double radio;
+    
+    public Circulo (double r) {
+        
+        if (r < 1 && r > 0)            
+            this.radio = r;
+        else
+            throw new IllegalArgumentException("Radio incorrecto");
+    }
+    
+    private double perimetro () {
+        return 2*Math.PI*getRadio();
+    }
+    
+    private double area () {
+        return Math.PI*Math.pow(getRadio(), 2);
+    }
+
+    public double getRadio() {
+        return radio;
+    }
+
+    public void setRadio(double radio) {
+        this.radio = radio;
+    }
+    
+    @Override
+    public String toString() {
+        return String.format("%s%.2f/t%s%.2f", "Perimetro: ", perimetro(),
+                "Area: ", area());
+    }
+}
--- src/Base/Circulos.java
+++
@@ -1,9 +0,0 @@
-package Base;
-
-public class Circulos {
-
-    public static void main(String[] args) {
-
-    }
-    
-}
